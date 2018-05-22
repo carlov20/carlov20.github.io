191df0e3e2cf7ebe9c273f40f5dc9c52eeec8371
carlov20 <carlo.a.veo@gmail.com>
2018-05-22 22:07:58 +0100

    update build.sh

--- build.sh
+++ build.sh
@@ -13,19 +13,19 @@ set -e
 jekyll build
 
 # cleanup
-rm -rf ../eshepelyuk.github.io.master
+rm -rf ../carlov20.github.io.master
 
 #clone `master' branch of the repository using encrypted GH_TOKEN for authentification
-git clone https://${GH_TOKEN}@github.com/eshepelyuk/eshepelyuk.github.io.git ../eshepelyuk.github.io.master
+git clone https://${GH_TOKEN}@github.com/carlov20/carlov20.github.io.git ../carlov20.github.io.master
 
 # copy generated HTML site to `master' branch
-cp -R _site/* ../eshepelyuk.github.io.master
+cp -R _site/* ../carlov20.github.io.master
 
 # commit and push generated content to `master' branch
 # since repository was cloned in write mode with token auth - we can push there
-cd ../eshepelyuk.github.io.master
-git config user.email "eshepelyuk@gmail.com"
-git config user.name "Evgeny Shepelyuk"
+cd ../carlov20.github.io.master
+git config user.email "carlo.a.veo@gmail.com"
+git config user.name "Carlo Veo"
 git add -A .
 git commit -a -m "Travis #$TRAVIS_BUILD_NUMBER"
 git push --quiet origin master > /dev/null 2>&1
